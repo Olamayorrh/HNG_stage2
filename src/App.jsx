c5bc690809b9ab53af07f3ca2d93d5099c07ba8d
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2024-07-11 23:06:43 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./Components/LandingPage/LandingPage";
 import Product from "./Components/Product/Product";
 import CheckOut from "./Components/CheckOut/CheckOut";
 import MyCartDisplay from "./Components/MyCart/MyCartDisplay";
+import MyCartFlexed from "./Components/MyCart/MyCartFlexed";
 
 
 const App = ()=> {
@@ -13,7 +14,7 @@ const App = ()=> {
 
       
           <Routes>
-              <Route path="/" element={<LandingPage/>}></Route>
+              <Route path="/" element={<MyCartDisplay/>}></Route>
               <Route path="/product/:productid" element={<Product/>}></Route>
               <Route path="/product/Mycart" element={<MyCartDisplay/>}></Route>
               <Route path="/product/CheckOut" element={<CheckOut/>}></Route>
